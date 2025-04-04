f18e3ad7fd64de79adcb4d11f2b44e272fc7b409
Marcio Costa <marcio-costa38@hotmail.com>
2025-04-04 00:37:13 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/devtech/DevTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.devtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devtech/DevTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
